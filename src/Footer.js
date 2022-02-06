c55d6912fa09dfe30f1bac341f2e404c91420c87
Ankur Seth <iankurseth@gmail.com>
2022-02-06 22:36:31 +0530

    Hardcode footer links * removed button, added text and name

--- src/Footer.js
+++ src/Footer.js
@@ -3,16 +3,13 @@
 
 export default function Footer ({links}) {
 
-	let linkItems = links.map((v, i) => {
-		return (
-			<a key={i} href={v.href} target="_blank" rel="noreferrer" className="px-4 py-3 bg-slate-600 cursor-pointer duration-300 hover:bg-slate-500">{v.title}</a>
-		);
-	});
+	let anchorClasses = "border-b-2 border-blue-500 duration-300 text-blue-500 hover:text-blue-200 hover:border-blue-200";
 
 	return (
-		<div className="Footer bg-slate-700 text-white px-2 py-12 text-center">
-			<div className="text-sm font-bold space-x-4 py-6">
-				{linkItems}
+		<div className="Footer bg-slate-700 text-white px-4 py-24 text-center">
+			<div className="text-md font-bold space-y-4">
+				<p>Designed and developed by <a href="https://github.com/iaseth" target="_blank" rel="noreferrer" className={anchorClasses}>Ankur Seth</a>.</p>
+				<p>Hosted on <a href="https://github.com/reactry/shades" target="_blank" rel="noreferrer" className={anchorClasses}>GitHub</a>.</p>
 			</div>
 		</div>
 	);
