5c1fb596baad4510d2ce80a74b3a4cff26a0d4cf
Ankur Seth <iankurseth@gmail.com>
2022-02-06 23:56:41 +0530

    Added Randomize color button

--- src/MainComponent/Input/BigBox.js
+++ src/MainComponent/Input/BigBox.js
@@ -12,8 +12,13 @@ export default function BigBox ({
 
 	return (
 		<div className="BigBox px-8 py-8">
-			<div className="h-40 md:h-56" style={style}></div>
-			<div className="bg-slate-100 px-4 py-4 text-slate-700 font-bold text-md">{hex}</div>
+			<div>
+				<div className="h-40 md:h-56" style={style}></div>
+				<div className="bg-slate-100 px-4 py-4 text-slate-700 font-bold text-md">{hex}</div>
+			</div>
+			<div className="pt-8 text-center flex">
+				<button className="bg-slate-100 px-16 py-4 text-xl border-2 border-slate-400 text-slate-700 rounded hover:bg-slate-100 focus:bg-slate-100">Randomize</button>
+			</div>
 		</div>
 	);
 }
