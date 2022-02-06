8a79dc9f9724e145c26a0ef95561c05e6faf387f
Ankur Seth <iankurseth@gmail.com>
2022-02-06 14:56:59 +0530

    Added top-border

--- src/MainComponent/MainComponent.js
+++ src/MainComponent/MainComponent.js
@@ -1,10 +1,20 @@
+import React from 'react';
 
 
 
 export default function MainComponent () {
 	return (
-		<main className="flex bg-purple-600 text-white min-h-screen">
-			<h2 className="text-[120px] m-auto">shades</h2>
-		</main>
+		<>
+			<div className="bg-purple-600 h-2 flex">
+				<div className="bg-red-500 grow"></div>
+				<div className="bg-slate-500 grow"></div>
+				<div className="bg-blue-500 grow"></div>
+				<div className="bg-slate-500 grow"></div>
+				<div className="bg-green-500 grow"></div>
+			</div>
+			<main className="flex bg-slate-600 text-white min-h-screen">
+				<h2 className="text-[120px] m-auto">shades</h2>
+			</main>
+		</>
 	);
 }
