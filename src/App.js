3d110c11bcba878a3a7dcfce70734b051100332f
Ankur Seth <iankurseth@gmail.com>
2022-02-06 22:14:01 +0530

    Moved RainbowBorder to root directory * added border above and below Thanks section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import RainbowBorder from './RainbowBorder';
 import MainComponent from './MainComponent';
 import Thanks from './Thanks';
 import Footer from './Footer';
@@ -15,10 +16,19 @@ function App () {
 	const thanks = settings.thanks;
 	let thanksProps = {thanks};
 
+	let bgClasses = [
+		"bg-red-500", "bg-slate-500",
+		"bg-blue-500", "bg-slate-500-500",
+		"bg-green-500"
+	];
+
 	return (
 		<div className="App">
+			<RainbowBorder bgClasses={bgClasses} />
 			<MainComponent {...mainProps} />
+			<RainbowBorder bgClasses={bgClasses} />
 			<Thanks {...thanksProps} />
+			<RainbowBorder bgClasses={bgClasses} />
 			<Footer {...footerProps} />
 		</div>
 	);
--- src/MainComponent/MainComponent.js
+++ src/MainComponent/MainComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import RainbowBorder from './RainbowBorder';
 import Branding from './Branding';
 import ColorInput from './Input';
 import ColorOutput from './Output';
@@ -14,12 +13,6 @@ export default function MainComponent ({settings}) {
 
 	let [n, setN] = React.useState(10);
 
-	let bgClasses = [
-		"bg-red-500", "bg-slate-500",
-		"bg-blue-500", "bg-slate-500-500",
-		"bg-green-500"
-	];
-
 	let colorInputProps = {
 		red, green, blue, n,
 		setRed, setGreen, setBlue, setN
@@ -30,7 +23,6 @@ export default function MainComponent ({settings}) {
 
 	return (
 		<>
-			<RainbowBorder bgClasses={bgClasses} />
 			<ColorInput {...colorInputProps} />
 			<ColorOutput {...colorOutputProps} />
 			<Branding meta={settings.meta} />
