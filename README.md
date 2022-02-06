51b3de7be72fe3b078747670bd8852678ae28a41
Ankur Seth <iankurseth@gmail.com>
2022-02-06 05:50:54 +0530

    c0 with cra, tailwind and cleanup

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Reactry's `shades`
+
+
+### goals
+* nothing yet
+
+
+### `1.0.0` - `future`
+* nothing yet
+
+
+### `0.1.0` - `2022-02-06`
+* nothing yet
+
+
---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App () {
+	return (
+		<div className="App">
+			<header className="flex bg-purple-600 text-white min-h-screen">
+				<h2 className="text-[120px] m-auto">shades</h2>
+			</header>
+		</div>
+	);
+}
+
+export default App;
