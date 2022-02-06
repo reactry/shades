9efe576b4115cb7b79ee79193e3b6a9bae8b36e4
Ankur Seth <iankurseth@gmail.com>
2022-02-06 23:45:16 +0530

    Set default color to rgb(120, 70, 230)

--- src/MainComponent/MainComponent.js
+++ src/MainComponent/MainComponent.js
@@ -6,9 +6,9 @@ import ColorOutput from './Output';
 
 
 export default function MainComponent ({settings}) {
-	let [red, setRed] = React.useState(128);
-	let [green, setGreen] = React.useState(128);
-	let [blue, setBlue] = React.useState(128);
+	let [red, setRed] = React.useState(120);
+	let [green, setGreen] = React.useState(70);
+	let [blue, setBlue] = React.useState(230);
 
 	let [n, setN] = React.useState(10);
 
