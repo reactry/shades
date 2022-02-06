e3d2cfeaf064c1cfa80f947882de94d28a778d1e
Ankur Seth <iankurseth@gmail.com>
2022-02-07 05:01:38 +0530

    Added Show/Hide button for shortcuts

--- src/KeyboardShortcuts.js
+++ src/KeyboardShortcuts.js
@@ -1,7 +1,10 @@
+import React from 'react';
 
 
 
 export default function KeyboardShortcuts ({keyboardShortcuts}) {
+	let [showShortcuts, setShowShortcuts] = React.useState(false);
+
 	let shortcutItems = keyboardShortcuts.map((v, i) => {
 		let keyClass = v.keyCode ? "bg-slate-300 px-3 py-2 w-12 text-center" : "py-2";
 
@@ -24,9 +27,14 @@ export default function KeyboardShortcuts ({keyboardShortcuts}) {
 	return (
 		<div className="KeyboardShortcuts bg-slate-300 text-slate-700 px-4 py-24">
 			<div className="max-w-5xl m-auto">
-				<h2 className="text-3xl px-4">Keyboard Shortcuts</h2>
-				<div className="py-8">
-					{shortcutItems}
+				<div className="px-4 py-8 flex">
+					<div onClick={() => setShowShortcuts(!showShortcuts)}
+						className="bg-slate-200 px-3 py-4 w-32 text-center text-xl border-2 border-slate-400 cursor-pointer text-slate-700 rounded hover:bg-slate-100 focus:bg-slate-100">
+						{showShortcuts ? "Hide" : "Show"}</div>
+					<div className="text-3xl pl-5 pt-4">Shortcuts</div>
+				</div>
+				<div className="px-4 py-8">
+					{showShortcuts && shortcutItems}
 				</div>
 			</div>
 		</div>
