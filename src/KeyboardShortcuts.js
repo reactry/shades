62f6835eacf52f00066b9e6eef0ef2972166b922
Ankur Seth <iankurseth@gmail.com>
2022-02-07 03:53:24 +0530

    Added planned shortcuts data to JSON

--- src/KeyboardShortcuts.js
+++ src/KeyboardShortcuts.js
@@ -3,9 +3,9 @@
 
 export default function KeyboardShortcuts () {
 	return (
-		<div className="KeyboardShortcuts bg-slate-300 px-4 py-16">
+		<div className="KeyboardShortcuts bg-slate-300 px-4 py-24">
 			<div className="max-w-5xl m-auto">
-				<div>KeyboardShortcuts</div>
+				<h2 className="text-3xl">Keyboard Shortcuts</h2>
 			</div>
 		</div>
 	);
--- src/Preferences.js
+++ src/Preferences.js
@@ -3,9 +3,9 @@
 
 export default function Preferences () {
 	return (
-		<div className="Preferences px-4 py-16 bg-slate-700 text-slate-200">
+		<div className="Preferences px-4 py-24 bg-slate-700 text-slate-200">
 			<div className="max-w-5xl m-auto">
-				<div>Preferences</div>
+				<h2 className="text-3xl">Preferences</h2>
 			</div>
 		</div>
 	);
