cffb1c185b4d89d5dbda25492061612b09d12234
Ankur Seth <iankurseth@gmail.com>
2022-02-07 19:35:41 +0530

    Minor styling changes

--- src/Branding.js
+++ src/Branding.js
@@ -3,12 +3,12 @@
 
 export default function Branding ({meta}) {
 	return (
-		<header className="px-4 py-16 bg-slate-700 shadow-xl">
-			<a className="block bg-white max-w-sm m-auto text-center" href="https://reactry.github.io/shades/">
+		<header className="px-4 py-24 bg-slate-700 shadow-xl">
+			<a className="block bg-white max-w-sm m-auto text-center duration-300 text-slate-400 hover:bg-blue-200 hover:text-slate-700" href="https://reactry.github.io/shades/">
 				<div className="flex justify-center">
-					<h3 className="px-12 py-2 text-md font-bold bg-slate-300">Reactry's</h3>
+					<h3 className="px-12 py-1 text-md font-bold border-x-2 border-b-2 border-slate-400 bg-slate-300 text-slate-500">Reactry's</h3>
 				</div>
-				<h1 className="text-[72px] px-4 pb-1 text-slate-400 border-b-2 border-slate-400">{meta.title}</h1>
+				<h1 className="text-[72px] px-4 pb-1 border-b-2 border-slate-400">{meta.title}</h1>
 				<p className="text-sm py-2 bg-slate-100 text-slate-600">{meta.tagline}</p>
 			</a>
 		</header>
--- src/MainComponent/Input/BigBox.js
+++ src/MainComponent/Input/BigBox.js
@@ -17,15 +17,15 @@ export default function BigBox ({
 
 	return (
 		<div className="BigBox px-8 py-8">
-			<div className="pb-8 flex">
+			<div className="flex">
 				<BigButton title="Darken" handleClick={darkenColor} />
 				<BigButton title="Lighten" handleClick={lightenColor} />
 			</div>
-			<div>
+			<div className="py-5">
 				<div className="h-40 md:h-56" style={style}></div>
 				<div className="bg-slate-100 px-4 py-4 text-slate-700 font-bold text-xl">{hex}</div>
 			</div>
-			<div className="pt-8 flex">
+			<div className="flex">
 				<BigButton title="Invert" handleClick={invertColor} />
 				<BigButton title="Randomize" handleClick={randomizeColor} />
 			</div>
