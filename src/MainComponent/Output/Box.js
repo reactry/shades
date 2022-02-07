1ba09d7ff20389a949b055b955af86cf5e5fb6b2
Ankur Seth <iankurseth@gmail.com>
2022-02-07 18:19:02 +0530

    Added 2 squares at top to active colorbox

--- src/MainComponent/Output/Box.js
+++ src/MainComponent/Output/Box.js
@@ -13,6 +13,10 @@ export default function Box ({r, g, b, active}) {
 
 	return (
 		<div className={boxClasses}>
+			{active && <div className="relative -top-1">
+				<div className="absolute -left-1 w-6 h-6 border-4 border-blue-600"></div>
+				<div className="absolute -right-1 w-6 h-6 border-4 border-blue-600"></div>
+			</div>}
 			<div className="h-16" style={style}></div>
 			<div className="bg-slate-100 py-2 text-sm text-center font-bold text-slate-700 shadow">{hexColor}</div>
 		</div>
