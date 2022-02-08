255e4868df029af0f5c2ae1f08e1c5ab8644290e
Ankur Seth <iankurseth@gmail.com>
2022-02-08 06:11:05 +0530

    Now using native input sliders in NumberSlider * added custom CSS slider-thumb

--- src/MainComponent/Input/NumberSlider.js
+++ src/MainComponent/Input/NumberSlider.js
@@ -1,5 +1,7 @@
 import {roundOffComponent} from '../../Utils';
 
+import './NumberSlider.css';
+
 
 
 export default function NumberSlider ({
@@ -17,6 +19,7 @@ export default function NumberSlider ({
 	}
 
 	function handleKeyDown (e) {
+		return;
 		let keyCode = e.keyCode;
 		if (keyCode === 37) {
 			// left arrow
@@ -34,13 +37,19 @@ export default function NumberSlider ({
 	let style = {
 		width: Math.floor((x/max) * 100) + "%"
 	}
-	let parentClass = bgPassive + " cursor-pointer";
+	let parentClass = bgPassive + " cursor-pointer hidden";
+	let sliderClass = `appearance-none ${bgActive} outline-none w-full form-range rounded`;
 
 	return (
-		<div className="NumberSlider px-4 py-3 font-bold text-sm outline-none border-2 border-transparent focus:bg-slate-200 focus:border-slate-400"
+		<div className="NumberSlider px-4 py-2 font-bold text-sm outline-none border-2 border-transparent focus:bg-slate-200 focus:border-slate-400"
 			tabIndex="0" onKeyDown={handleKeyDown}>
 			<div>
-				<h2 className="mb-1 px-1">{title}</h2>
+				<h2 className="mb-2 px-1">{title}</h2>
+				<div className="NumberSlider">
+					<input type="range" min="0" max="255" value={x}
+						className={sliderClass}
+						onChange={(e) => setX(e.target.value)} />
+				</div>
 				<div className={parentClass} onClick={handleClick}>
 					<div className={"h-4 relative " + bgActive} style={style}>
 						<div className="absolute -right-1 -top-1 h-6 w-2 bg-slate-600 border-2 border-white outline outline-2 outline-slate-800"></div>
